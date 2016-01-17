729da358839ba9ffee111c2f0846e0bc7cd27a03
sujon335 <sujon335@yahoo.com>
2016-01-16 22:50:02 -0800

    order quantity

--- application/controllers/checkout.php
+++ application/controllers/checkout.php
@@ -12,11 +12,44 @@
  * @author dripto
  */
 class Checkout extends CI_Controller {
+
     //put your code here
-    public function index(){
-        $header_data['header_name'] ="checkout"; 
+    public function index() {
+        $is_logged_in = $this->session->userdata('is_logged_in_user');
+        if (!isset($is_logged_in) || $is_logged_in != TRUE)
+            redirect('login');
+        $user_id=$this->session->userdata('user_id');
+        $data=array();
+        $this->db->join('products', 'products.id=design.product_id');
+        $this->db->where('design.user_id', $user_id);
+        $query = $this->db->get('design');
+        $data['design'] = $query->result();
+        $header_data['header_name'] = "checkout";
         $this->load->view("common/header", $header_data);
-        $this->load->view("checkout/checkout_body");
-        $this->load->view("common/footer"); 
+        $this->load->view("checkout/checkout_body",$data);
+        $this->load->view("common/footer");
     }
+    public function save_order_design()
+    {
+        $user_id=$this->session->userdata('user_id');
+        $order_data = $this->input->post("order_data");
+        $obj = json_decode($order_data);
+
+        for ($i = 0; $i < sizeof($obj); $i++) {
+            $var = $obj["$i"];
+            $design_id=$var->design_id;
+            $quantity=$var->quantity;
+            $array = array(
+                'user_id'=>$user_id,
+                'design_id' => $design_id,
+                'quantity' => $quantity
+            );
+            $in = $this->db->insert("temp_order_design", $array);
+        }
+    }
+    public function save_order()
+    {
+        
+    }
+
 }
--- application/views/my_cards/my_cards_body.php
+++ application/views/my_cards/my_cards_body.php
@@ -27,9 +27,10 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php if (isset($design)) {
+                    <?php
+                    if (isset($design)) {
                         foreach ($design as $row) {
- ?>
+                    ?>
                             <tr>
                                 <td class="cart_product">
                                     <a href=""><img src="<?php echo base_url(); ?>uploads/<?php echo $row->front_side; ?>" height="100" width="auto" alt=""></a>
@@ -45,9 +46,9 @@
                                 </td>
                                 <td class="cart_quantity">
                                     <div class="cart_quantity_button">
-                                        <a class="cart_quantity_up" href=""> + </a>
-                                        <input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-                                        <a class="cart_quantity_down" href=""> - </a>
+                                        <a id="<?php echo $row->design_id; ?>" class="cart_quantity_up" href=""> + </a>
+                                        <input id="quantity-<?php echo $row->design_id; ?>" class="cart_quantity_input" type="text" name="quantity" value="1"  size="2">
+                                        <a id="<?php echo $row->design_id; ?>" class="cart_quantity_down" href=""> - </a>
                                     </div>
                                 </td>
                                 <td class="cart_total">
@@ -58,64 +59,129 @@
                                 </td>
                             </tr>
 
- <div id="d-<?php echo $row->design_id; ?>" class="modal fade" role="dialog">
-                <div class="modal-dialog">
+                        <div id="d-<?php echo $row->design_id; ?>" class="modal fade" role="dialog">
+                            <div class="modal-dialog">
 
-                    <!-- Modal content-->
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">Modal Header</h4>
-                        </div>
-                        <div class="modal-body">
-                            <div id="myNicPanel" >
-                                <div class="view-product" id="frontcard" style="position:relative">
-                                    <img src="<?php echo base_url(); ?>uploads/<?php echo $row->front_side; ?>" alt="" />
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <h4 class="modal-title">Modal Header</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div id="myNicPanel" >
+                                            <div class="view-product" id="frontcard" style="position:relative">
+                                                <img src="<?php echo base_url(); ?>uploads/<?php echo $row->front_side; ?>" alt="" />
 
-                    
 
-                                    <h3>ZOOM</h3>
+
+                                                <h3>ZOOM</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        </div>
-                    </div>
 
-                </div>
+
+                <?php }
+                    } ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="">
+                <a id="checkout_btn" class="btn btn-lg btn-primary pull-right" href="<?php echo base_url(); ?>index.php/checkout">Checkout</a>
+
             </div>
 
-
-                    <?php }
-                    } ?>
-                </tbody>
-            </table>
-           
-
         </div>
-    </div>
-</section> <!--/#cart_items-->
+    </section> <!--/#cart_items-->
+    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery-ui.js"></script>
 
-<script type="text/javascript">
+    <script type="text/javascript">
+
+        $(document).ready(function() {
+            $(".cart_quantity_up").click(function(event) {
+                event.preventDefault();
+                var id=this.id;
+                var value=$("#quantity-"+id).val();
+                value++;
+                $("#quantity-"+id).val(value);
+            });
+            $(".cart_quantity_down").click(function(event) {
+                event.preventDefault();
+                var id=this.id;
+                var value=$("#quantity-"+id).val();
+                value--;
+                if(value<1)
+                    value=1;
+                $("#quantity-"+id).val(value);
+            });
+        });
 
 
-function get_text(id) {
-                        
+
+
+
+
+        function get_text(id) {
+
+
+            jQuery.ajax({
+                type: "POST",
+                url: "<?php echo base_url(); ?>" + "index.php/myCards/get_card_text_data",
+            data:  {design_id: id},
+            success: function(data) {
+                alert('success : '+data);
+                $.each(data, function (key, item) {
+                    // Add a list item for the product.
+                      
+                });
+            }
+        });
+
+    }
+
+</script>
+
+ <script type="text/javascript">
+
+                // Ajax post
+                $(document).ready(function() {
+                    $("#checkout_btn").click(function(event) {
+                      // event.preventDefault();
+                       var JsonObj=[];
+                    <?php
+                    if (isset($design)) {
+                        foreach ($design as $row) {
+                    ?>
+                            var left=$(this).css('left');
+
+                            var item = {};
+                            item ["design_id"] = <?php echo $row->design_id ?>;
+                            var id=<?php echo $row->design_id ?>;
+                            item ["quantity"] = $("#quantity-"+id).val();
+                            JsonObj.push(item);
+                        <?php }} ?>
+
+                        var jsonString=JSON.stringify(JsonObj);
+
+                        //alert(jsonString);
 
                         jQuery.ajax({
                             type: "POST",
-                            url: "<?php echo base_url(); ?>" + "index.php/myCards/get_card_text_data",
-                data:  {design_id: id},
+                            url: "<?php echo base_url(); ?>" + "index.php/checkout/save_order_design",
+                data:  {order_data: jsonString},
                 success: function(data) {
-                    alert('success : '+data);
-                    $.each(data, function (key, item) {
-                        // Add a list item for the product.
-                      
-                    });                  
+                    //alert('success json data: '+data);
                 }
             });
-
-        }
-
+        });
+    });
 </script>
